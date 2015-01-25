f94ef487ac5865fb20b4935f0f16845c464802b3
arnaud sellenet <arnodmental@gmail.com>
2015-01-25 22:36:53 +0100

    invalid thing#create spec

--- spec/controllers/things_controller_spec.rb
+++ spec/controllers/things_controller_spec.rb
@@ -1,11 +1,15 @@
 
 describe ThingsController, type: :controller do
+  render_views
+
   describe '#create' do
     it do #valid
       expect(post :create, thing: {name: "Bad Religion"}).to redirect_to thing_path(Thing.last)
     end
 
     it do #invalid
+      post :create, thing: {name: ""}
+      expect(response.body).to have_selector('.error')
     end
   end
 end
